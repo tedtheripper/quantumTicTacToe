babc0325143b5a2cf674c0998b73f3aa5d201add
tedtheripper <lemon.juice0731@gmail.com>
2020-01-18 20:44:03 +0100

    Many very important changes

--- src/graph.py
+++ src/graph.py
@@ -1,6 +1,14 @@
 from vertex import Vertex
 
 
+class BadArgumentError(Exception):
+    pass
+
+
+class GraphTooSmallError(Exception):
+    pass
+
+
 class Graph:  # Graph
     '''
     Class Graph. Represents relations which are costructed on the gameboard
@@ -15,10 +23,18 @@ class Graph:  # Graph
 
     def add_vertex(self, inc_id: int, square_index: int) -> None:
         # Adding new node to the graph
+        if type(inc_id) != int or type(square_index) != int:
+            raise BadArgumentError
+        if inc_id in self.vertices.keys():
+            raise BadArgumentError
+        if square_index >= 9 or square_index < 0:
+            raise BadArgumentError
         self.vertices[inc_id] = Vertex(inc_id, square_index)
 
     def remove_vertex(self, inc_id: int) -> None:
         # Removing existing node from the graph
+        if type(inc_id) != int or inc_id not in self.vertices.keys():
+            raise BadArgumentError
         for n in self.vertices[inc_id].get_neighbours():
             self.vertices[n].remove_neighbour(inc_id)
         self.vertices[inc_id].clean_neighbours()
@@ -26,6 +42,12 @@ class Graph:  # Graph
 
     def add_edge(self, id1: int, id2: int) -> None:
         # Adding edges to nodes in the graph
+        if type(id1) != int or type(id2) != int:
+            raise BadArgumentError
+        if id1 not in self.vertices.keys() or id2 not in self.vertices.keys():
+            raise BadArgumentError
+        if id1 in self.vertices[id2].get_neighbours() or id2 in self.vertices[id1].get_neighbours():
+            raise BadArgumentError
         self.vertices[id1].add_neighbour(id2)
         self.vertices[id2].add_neighbour(id1)
 
@@ -35,10 +57,18 @@ class Graph:  # Graph
 
     def get_neighbours(self, id: int) -> list:
         # Returns all neighbours of given node
+        if type(id) != int:
+            raise BadArgumentError
+        if id not in self.vertices.keys():
+            raise BadArgumentError
         return self.vertices[id].get_neighbours()
 
     def get_square_index(self, id: int) -> int:
         # Returns index of square containing given node
+        if type(id) != int:
+            raise BadArgumentError
+        if id not in self.vertices.keys():
+            raise BadArgumentError
         return self.vertices[id].get_index()
 
     def get_board(self) -> list:
@@ -50,6 +80,10 @@ class Graph:  # Graph
 
     def get_all_vertices_in_given_square(self, square_ix: int) -> list:
         # Returns every node in given square on the board
+        if type(square_ix) != int:
+            raise BadArgumentError
+        if square_ix >= 9 or square_ix < 0:
+            raise BadArgumentError
         result = []
         for v in self.vertices.keys():
             if self.vertices[v].get_index() == square_ix:
@@ -77,12 +111,13 @@ class Graph:  # Graph
         for n in self.vertices[v].get_neighbours():
             if not visited[n]:
                 if self.is_cyclic_utility(n, visited, v, cycle_elements, last):
-                    cycle_elements.append(n)
+                    cycle_elements.append(v)
                     if len(last) > 0:
                         if n == last[0]:
                             last.remove(n)
                     if len(last) > 0:
-                        cycle_elements.append(v)
+                        # cycle_elements.append(v)
+                        pass
                     return True
             elif parent != n:
                 last.append(n)
@@ -90,7 +125,7 @@ class Graph:  # Graph
                 return True
         return False
 
-    def all_in_one_square(self, cycled):
+    def all_in_one_square(self, cycled: list) -> bool:
         for sq in self.get_board():
             global full
             full = True
@@ -117,12 +152,14 @@ class Graph:  # Graph
             cycle_elements = []
             if not visited[v]:
                 if self.is_cyclic_utility(v, visited, -1, cycle_elements, last):
+                    result = self.get_correct_cycle(cycle_elements)
                     print(cycle_elements)
-                    if self.all_in_one_square(cycle_elements):
-                        print('Cycle is inside one square')
+                    print(result)
+                    if self.all_in_one_square(result) or len(result) < 3:
+                        # print('Cycle is inside one square')
                         has_cycle = False
                     else:
-                        return True, cycle_elements
+                        return True, result
         if not has_cycle:
             return False, None
 
@@ -149,9 +186,12 @@ class Graph:  # Graph
         # Returns list of correct squares to choose from when collapse is happening
         vert = self.get_correct_cycle(cycle)
         square = self.get_all_vertices_in_given_square(id)
+        to_be_removed = []
         for item in square:
             if item not in vert:
-                square.remove(item)
+                to_be_removed.append(item)
+        for item in to_be_removed:
+            square.remove(item)
         return square
 
     def get_correct_cycle(self, cycle: list) -> list:
@@ -175,25 +215,7 @@ class Graph:  # Graph
         for v in to_be_removed:
             self.remove_vertex(v)
 
-    def handle_collapse(self, cycle: list, choice: int) -> None:
-        # Responsible for correct graph coloring (Blue, Red)
-        self.vertices[choice].set_color('Blue')
-        if choice % 2 == 0:
-            self.vertices[choice-1].set_color('Red')
-        else:
-            self.vertices[choice+1].set_color('Red')
-        while not self.all_colored_in_graph(cycle):
-            for v in cycle:
-                if self.vertices[v].get_color() == 'Blue':
-                    for n in self.vertices[v].get_neighbours():
-                        if not self.vertices[n].get_color():
-                            self.vertices[n].set_color('Red')
-                            if n % 2 == 0:
-                                if not self.vertices[n-1].get_color():
-                                    self.vertices[n-1].set_color('Blue')
-                            else:
-                                if not self.vertices[n+1].get_color():
-                                    self.vertices[n+1].set_color('Blue')
+    def handle_collapse_helper(self) -> None:
         board = self.get_board()
         for sq in board:
             vs = self.get_all_vertices_in_given_square(board.index(sq))
@@ -217,5 +239,32 @@ class Graph:  # Graph
                 else:
                     if v+1 in self.vertices.keys():
                         self.vertices[v+1].set_color('Blue')
+
+    def handle_collapse(self, cycle: list, choice: int) -> None:
+        # Responsible for correct graph coloring (Blue, Red)
+        self.vertices[choice].set_color('Blue')
+        if choice % 2 == 0:
+            self.vertices[choice-1].set_color('Red')
+        else:
+            self.vertices[choice+1].set_color('Red')
+        while not self.all_colored_in_graph(cycle):
+            for v in cycle:
+                if self.vertices[v].get_color() == 'Blue':
+                    for n in self.vertices[v].get_neighbours():
+                        if not self.vertices[n].get_color():
+                            self.vertices[n].set_color('Red')
+                            if n % 2 == 0:
+                                if not self.vertices[n-1].get_color():
+                                    self.vertices[n-1].set_color('Blue')
+                            else:
+                                if not self.vertices[n+1].get_color():
+                                    self.vertices[n+1].set_color('Blue')
+        self.show_colored_graph()
+        print('-------------')
+        for i in range(0, 3):
+            i = i
+            self.handle_collapse_helper()
+        self.show_colored_graph()
+        print('--------------------')
         self.handle_collapse_delete()
         self.show_colored_graph()
